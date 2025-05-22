44869c9fb4258075cea697798222dc97123c4061
serianoseri <serianoseri@gmail.com>
2025-05-22 18:21:51 +0200

    small tweaks to weapon, added camera, and HUD elements

--- actors/Player.gd
+++ actors/Player.gd
@@ -10,27 +10,30 @@ signal player_fired_bullet(bullet, transform)
 func _ready():
 	weapon.connect("weapon_fired", self.shoot)
 	emit_signal("update_ammo", weapon.mag_size)
+	print(get_tree().World_Boundaries.get_children())
 
 func _physics_process(_delta):
 	look_at(get_global_mouse_position())
-	
-	var direction = Input.get_vector("left","right","up","down")
+	var direction = Input.get_vector("left","right","up","down").normalized()
 	velocity = direction * speed
-	
 	move_and_slide()
+	
 	if Input.is_action_pressed("click"):
 		weapon.shoot()
-
-#func _unhandled_input(event):
-	#if event.is_action_released("click"):
-		#weapon.shoot()
+	
+	if Input.is_action_pressed("reload"):
+		weapon.reload()
 
 func shoot(bullet_instance, _transform):
 	emit_signal("player_fired_bullet", bullet_instance, _transform)
 	emit_signal("update_ammo", weapon.bullet_count)
 
-func handle_hit():
-	health_stat.health -= 20
+func reload():
+	weapon.reload_cooldown.is_stopped()
+
+func handle_hit(damage:int):
+	pass
+	#health_stat.health -= 20
 
 func is_dead():
 	return health_stat.is_dead()
--- weapons/Weapon.gd
+++ weapons/Weapon.gd
@@ -3,28 +3,38 @@ extends Node2D
 signal weapon_fired(bullet, _transform)
 signal update_ammo(amount: int)
 
+enum state{IDLE, SHOOTING, RELOADING, RUNNING}
+
 @export var Bullet:PackedScene
 
 @onready var end_of_gun = $EndOfGun
 @onready var attack_cooldown = $AttackCooldown
+@onready var reload_cooldown = $ReloadCooldown
+@onready var reload_progress = $reload_hud
 @onready var anim_player = $AnimationPlayer
-@export var wait_time:float = 0.2
+@export var attack_cooldown_time:float = 0.1
+@export var reload_cooldwon_time:float = 2.4
 @export var spray_value:float = 5
 @export var mag_size:int = 30
-@export var damage:int = 20
+@export var damage:int = 34
 var spray:Transform2D
 var bullet_count:int
 
 func _process(delta):
 	emit_signal("update_ammo", bullet_count)
+	if !reload_cooldown.is_stopped():
+		reload_progress.set_value(reload_cooldown.time_left)
 
 func _ready() -> void:
-	attack_cooldown.set_wait_time(wait_time)
+	attack_cooldown.set_wait_time(attack_cooldown_time)
+	reload_cooldown.set_wait_time(reload_cooldwon_time)
 	bullet_count = mag_size
-	emit_signal("update_ammo", mag_size)
+	$MuzzleFlash.visible = false
+	reload_progress.max_value = reload_cooldwon_time
+	reload_progress.visible = false
 
 func shoot():
-	if attack_cooldown.is_stopped() and Bullet != null and !(bullet_count == 0):
+	if attack_cooldown.is_stopped() and !(bullet_count == 0) and reload_cooldown.is_stopped():
 		var bullet_instance = Bullet.instantiate()
 		spray = end_of_gun.global_transform
 		var rand_spray = randf_range(-spray_value/2, spray_value/2)
@@ -38,6 +48,13 @@ func shoot():
 		print(bullet_count)
 		anim_player.play("muzzle_flash")
 
+func reload():
+	if reload_cooldown.is_stopped() and bullet_count < mag_size:
+		reload_progress.visible = true
+		reload_cooldown.start()
+		await reload_cooldown.timeout
+		bullet_count = mag_size
+		reload_progress.visible = false
+
 func decrease_bullet_count(decrease_amount:int):
 	bullet_count = clamp(bullet_count - decrease_amount, 0, mag_size)
-	emit_signal("update_ammo", bullet_count)
